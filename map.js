1564f3617cf2e966c14c92fd7b7f52b91a3172fa
AndrosDe <andreas_beier1981@yahoo.de>
2022-04-28 13:39:57 +0000

    initial commit

--- map.js
+++ map.js
@@ -5,15 +5,31 @@
  */
 
 // Using a for loop
-
+let nums = [1, 2, 3, 4, 5];
+let results = [];
+for (let num of nums){
+  results.push(num*2)
+};
+console.log("For-Loop:", results);
+console.log("");
 
 // Using map()
-
+const multByTwo = function (num) {
+  return num*2;
+}
+const mapResults = nums.map(multByTwo)
+console.log("mapResults", mapResults);
+console.log("");
 
 // Simplified w/ map()
-
+const SimplemapResults = nums.map(function (num) {return num*2;});
+console.log("SimplemapResults", SimplemapResults);
+console.log("");
 
 // Simplfied w/ map() + arrow function
+const simplified = nums.map(num => num*2);
+console.log("Simplified MapResults", simplified);
+console.log("");
 
 
 // With objects:
@@ -37,3 +53,6 @@ const students = [
     skill: 'CSS'
   },
 ];
+
+const studentsWithIds = students.map(student => [student.name, student.id]);
+console.log("Object Map", studentsWithIds);
